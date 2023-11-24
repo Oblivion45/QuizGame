1a8764a822bfcfb6f3ae44c24f264f0c770c1fa3
Rizqi Rifaldi <rizqirifaldi80@gmail.com>
2023-11-25 06:22:38 +0700

    Main

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1700868074,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/buttona-sheet0.png",
+		"images/startbutton-sheet0.png",
+		"images/createroombutton-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
